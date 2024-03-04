8f7715b4cd27507c9c9c2646724a79fc0359f0cf
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-03-04 11:38:12 +0000

    final brainwave website

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Benefit from "./components/Benefit"
+import Collaboration from "./components/Collaboration"
+import Services from "./components/Services"
+import "./index.css"
+import Pricing from "./components/Pricing"
+import Roadmap from "./components/Roadmap"
+import Footer from "./components/Footer"
+
+
+const App = () => {
+  return (
+   <>
+   <div className="pt-[4.7rem] lg:pt-[5.25rem] overflow-hidden">
+   <Header/>
+   <Hero/>
+   <Benefit/>
+   <Collaboration/>
+   <Services/>
+   <Pricing/>
+   <Roadmap/>
+   <Footer/>
+   </div>
+
+   <ButtonGradient />
+   </>
+  )
+}
+
+export default App
